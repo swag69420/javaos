0c632c300259ef548ec2db4aa8d98cb5f90a696e
swag69420 <88593082+swag69420@users.noreply.github.com>
2021-08-15 01:57:47 -0700

    Update Boot.java
    work

--- src/kernel/Boot.java
+++ src/kernel/Boot.java
@@ -13,4 +13,17 @@ public class Boot {
         err.println("usage: Boot" + " <cacheSize> <diskName> <diskSize> <shell>" + " [ <shell parameters> ...]");
         exit(-1);
     }
+
+    public static void main(String args[]){
+        if (args.length < 4) {
+            usage();
+        }
+
+        int cacheSize = Integer.parseInt(args[0]);
+        String diskName = args[1];
+        int diskSize = Integer.parseInt(args[2]);
+        StringBuffer shellCommand = new StringBuffer(args[3]);
+
+        for (int i = 4; i < args.length;)
+    }
 }
